16bf8dc38a391295e4b15f690e8d9a827f39a84e
Timofey Prokofyev <tryatim8@gmail.com>
2025-03-11 15:14:04 +0400

    add static urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -55,6 +55,9 @@ if settings.DEBUG:
     urlpatterns.extend(
         static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     )
+    urlpatterns.extend(
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    )
     urlpatterns.append(
         path('__debug__', include('debug_toolbar.urls')),
     )
